aa55b4995177473004b2098b7b1abf89ec745d97
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2024-05-06 08:54:03 +0900

    Initial commit

---
+++ src/main/java/mumeinosato/minecraftapiexample/Minecraft_api_example.java
@@ -0,0 +1,17 @@
+package mumeinosato.minecraftapiexample;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Minecraft_api_example extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
